0f7aa72ad721bed61d257dd69e415a44c355a9a2
Thomas Voss <mail@thomasvoss.com>
2022-11-24 02:29:56 +0100

    Use less(1) as a pager for task-ls output

--- src/task-ls.c
+++ src/task-ls.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #include <dirent.h>
 #include <err.h>
@@ -7,6 +8,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "task.h"
 
@@ -15,9 +17,15 @@ struct task_vector {
 	struct task *tasks;
 };
 
+enum pipe_ends {
+	READ,
+	WRITE
+};
+
 static int  sizetlen(size_t);
 static int  qsort_helper(const void *, const void *);
 static void lstasks(int);
+static void outputlist(struct task_vector);
 static void append(struct task_vector *, struct task);
 
 void
@@ -49,7 +57,7 @@ subcmdlist(int argc, char **argv, int *dfds)
 void
 lstasks(int dfd)
 {
-	int fd, pad;
+	int fd;
 	DIR *dp;
 	FILE *fp;
 	struct dirent *ent;
@@ -81,12 +89,43 @@ lstasks(int dfd)
 	qsort(vec.tasks, vec.len, sizeof(struct task), &qsort_helper);
 
 	if (vec.len > 0)
-		pad = sizetlen(vec.tasks[vec.len - 1].id);
-	for (size_t i = 0; i < vec.len; i++)
-		printf("%*zu. %s\n", pad, vec.tasks[i].id, vec.tasks[i].title);
+		outputlist(vec);
 
 	free(vec.tasks);
 	closedir(dp);
+}
+
+void
+outputlist(struct task_vector vec)
+{
+	int pad, fds[2];
+	pid_t pid;
+	FILE *fp;
+
+	if (pipe(fds) == -1)
+		err(EXIT_FAILURE, "pipe");
+	if ((pid = fork()) == -1)
+		err(EXIT_FAILURE, "fork");
+	if (pid == 0) {
+		close(fds[WRITE]);
+		close(STDIN_FILENO);
+		if (dup2(fds[READ], STDIN_FILENO) == -1)
+			err(EXIT_FAILURE, "dup2");
+		close(fds[READ]);
+		execlp("less", "less", "-FS", NULL);
+		err(EXIT_FAILURE, "execlp");
+	}
+
+	close(fds[READ]);
+	pad = sizetlen(vec.tasks[vec.len - 1].id);
+	if ((fp = fdopen(fds[WRITE], "w")) == NULL)
+		err(EXIT_FAILURE, "fdopen: Pipe to pager");
+	for (size_t i = 0; i < vec.len; i++)
+		fprintf(fp, "%*zu. %s\n", pad,
+		        vec.tasks[i].id, vec.tasks[i].title);
+	fclose(fp);
+	if (waitpid(pid, NULL, 0) == -1)
+		err(EXIT_FAILURE, "waitpid");
 }
 
 void
