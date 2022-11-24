7f270b74352a7cd0d5350d3b8f645ecc1f124150
Thomas Voss <mail@thomasvoss.com>
2022-11-24 02:37:34 +0100

    Don't include string.h with streq() macro

--- src/common.h
+++ src/common.h
@@ -1,8 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <string.h>
-
 #define streq(x, y) (strcmp(x, y) == 0)
 
 void  errtoolong(const char *, ...);
--- src/task-ls.c
+++ src/task-ls.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "task.h"
